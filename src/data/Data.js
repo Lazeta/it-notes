8361f17d97563008aabc4a26a2939080aec0c82c
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2025-01-26 20:41:55 +0300

    feat: update task 4 from JS

--- src/data/Data.js
+++ src/data/Data.js
@@ -5583,6 +5583,39 @@ function correctPolishLetters (string) {
   
   return result.join('')
 }
+
+// or
+
+function correctPolishLetters (str) {
+  return str
+  .replace(/ą/g, 'a')
+  .replace(/ć/g, 'c')
+  .replace(/ę/g, 'e')
+  .replace(/ł/g, 'l')
+  .replace(/ń/g, 'n')
+  .replace(/ó/g, 'o')
+  .replace(/ś/g, 's')
+  .replace(/ź/g, 'z')
+  .replace(/ż/g, 'z')
+}
+
+// or
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(correctPolishLetters("Jędrzej Błądziński"), "Jedrzej Bladzinski");
+    assert.strictEqual(correctPolishLetters("Lech Wałęsa"), "Lech Walesa");
+    assert.strictEqual(correctPolishLetters("Maria Skłodowska-Curie"), "Maria Sklodowska-Curie");
+  });
+});
+
+// or
+
+const correctPolishLetters = str => {
+  const diacritics = {'ą':'a','ć':'c','ę':'e','ł':'l','ń':'n','ó':'o','ś':'s','ź':'z','ż':'z'};
+
+  return str.replace(/[ąćęłńóśźż]/g, match => diacritics[match])
+}
                   `,
                 }
               ]
