63c54fd5daf6bb5e2280e00ca0938441f7c1662a
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2025-03-24 00:16:00 +0300

    added git topic

---
+++ src/data/git/gitParent.js
@@ -0,0 +1,14 @@
+import { Git } from "./Git/git";
+import { gitCommands } from "./gitCommands/gitCommands";
+import { gitFlow } from "./gitFlow/gitFlow";
+
+export const gitParent = {
+    id: 20,
+    title: "Git",
+    children: [
+      Git,
+      gitFlow,
+      gitCommands,
+    ],
+  };
+  
