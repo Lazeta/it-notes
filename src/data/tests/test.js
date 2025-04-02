da467ee1979c578c5dcebc6c67bfca52d5373739
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2025-04-02 13:40:01 +0300

    feat: added new both tests (REST & CORS) and correct these theme topics

--- src/data/tests/test.js
+++ src/data/tests/test.js
@@ -1,4 +1,6 @@
 import { test1 } from "./test1";
 import { test2 } from "./test2";
+import { test3 } from "./test3";
+import { test4 } from "./test4";
 
-export const test = [test1, test2]
+export const test = [test1, test2, test3, test4];
