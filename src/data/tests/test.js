bb2cbdb216a79318ebe8bd9edbf3c24fed616af2
Check Stanislau <fkrjcjdjifkdgdjsjjfmknckdnc@gmail.com>
2025-04-17 17:41:07 +0300

    feat: added two test backend

--- src/data/tests/test.js
+++ src/data/tests/test.js
@@ -2,5 +2,7 @@ import { test1 } from "./test1";
 import { test2 } from "./test2";
 import { test3 } from "./test3";
 import { test4 } from "./test4";
+import { testRequestParamJSON } from "./testRequestParamJSON";
+import { testWorkWithBackend } from "./testWorkWithBackend";
 
-export const test = [test1, test2, test3, test4];
+export const test = [test1, test2, test3, test4, testWorkWithBackend, testRequestParamJSON,];
---
+++ src/data/tests/testRequestParamJSON.js
@@ -0,0 +1,23 @@
+export const testRequestParamJSON = {
+  id: 100075,
+  title: "Request parameters and JSON",
+  questions: [
+    {
+      id: 100085,
+      question: "Как клонировать объект в JavaScript с использованием JSON?",
+      answer: `
+      
+      `,
+    },
+    {
+      id: 100086,
+      question: "Как добавить тело запроса в HTTP-запрос при использовании fetch API?",
+      answer: ``,
+    },
+    {
+      id: 100087,
+      question: "Какие типы содержимого можно использовать при отправке HTTP-запроса, и как это влияет на запрос и ответ?",
+      answer: ``,
+    }
+  ],
+};
